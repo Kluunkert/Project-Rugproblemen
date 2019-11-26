f81640890e61bc99b029463ed42989a32ad2f04a
wtfRowin <r.s.salomons@gmail.com>
2019-11-26 19:16:23 +0100

    Convo shit

---
+++ RugProbleem/Assets/Rowin/Rowin Scripts/AnswerButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerButton : MonoBehaviour
+{
+   public ConvoObject nextConvo;
+   protected ConversationSystem conversationSystem;
+
+   public void Start()
+   {
+       conversationSystem = GameObject.Find("ConversationManager").GetComponent<ConversationSystem>();
+   }
+
+   public void OnClickButton()
+   {
+       if(nextConvo != null)
+       {
+        conversationSystem.mainConvo = nextConvo;
+        
+       }
+       else
+       {
+           print("There is no next conversation");
+       }
+   }
+}
---
+++ RugProbleem/Assets/Rowin/Rowin Scripts/ConversationSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ConversationSystem : MonoBehaviour
+{
+    public ConvoObject mainConvo, nextConvo;
+    public TextMeshProUGUI questionText, knowledgeText;
+    public TextMeshProUGUI[] buttonsText;
+
+    public void Start()
+    {
+        questionText.text = mainConvo.question;
+
+        buttonsText[0].text = mainConvo.answers[0];
+        buttonsText[1].text = mainConvo.answers[1];
+        buttonsText[2].text = mainConvo.answers[2];
+        buttonsText[3].text = mainConvo.answers[3];
+    }
+}
