c4737c389bfc4bd27f1f2ddcfcdbaffa667738e6
luuk boersma <49709526+Kluunkert@users.noreply.github.com>
2019-11-29 11:01:27 +0100

    NoteBlockGang :100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: vv
    100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire: 100: :fire:

---
+++ RugProbleem/Assets/Scripts/Luuk/NotitieCode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotitieCode : MonoBehaviour
+{
+    public List<GameObject> hidden;
+    public List<GameObject> pages;
+
+    public int pageInt;
+    public int unlocked;
+
+    public void Update()
+    {
+        Unhide();
+        PageChange();
+
+    }
+
+    public void Unhide()
+    {
+        hidden[unlocked].SetActive(true);
+    }
+
+    public void PageChange()
+    {
+        if (Input.GetButton("NextPage"))
+        {
+            pageInt++;
+        }
+        if (Input.GetButton("PrevPage"))
+        {
+            pageInt--;
+        }
+
+    }
+}
