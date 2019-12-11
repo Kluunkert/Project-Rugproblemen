a48335036b0101d2930770c9b59a9f01011bf233
luuk boersma <49709526+Kluunkert@users.noreply.github.com>
2019-12-11 13:25:12 +0100

    Eskittit

---
+++ RugProbleem/Assets/3 - Scripts/Luuk/FirstPersonCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCam : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    public Rigidbody rig;
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        transform.Translate(hor, 0, ver);
+    }
+}
