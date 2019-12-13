6e9d9b8fc108390ef13891f9abbc1b5d909f7936
wtfRowin <r.s.salomons@gmail.com>
2019-12-13 14:31:35 +0100

    dasdas

--- RugProbleem/Assets/2 - Rowin/Rowin Scripts/ButtonScript.cs
+++ RugProbleem/Assets/2 - Rowin/Rowin Scripts/ButtonScript.cs
@@ -4,14 +4,5 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
-    public GameObject showButtons;
-    public bool showButtons_;
-
-    void Update()
-    {
-        if(showButtons_)
-        {
-            showButtons.SetActive(true);
-        }
-    }
+   public ScriptableObject question;
 }
