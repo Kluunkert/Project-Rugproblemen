7e2037d555302a4f20c7841b79e9f3674cec901d
luuk boersma <49709526+Kluunkert@users.noreply.github.com>
2019-12-16 16:48:17 +0100

    Poppin'up

---
+++ RugProbleem/Assets/3 - Scripts/Luuk/TutorialPopUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPopUp : MonoBehaviour
+{
+    public List<GameObject> pep;
+    public List<bool> high; 
+
+    public GameObject kluis;
+
+
+    public void Awake()
+    {
+        kluis = gameObject;
+    }
+
+    public void Snuiven(int gram)
+    {
+        Instantiate(pep[gram], transform.position, Quaternion.identity, kluis.transform);
+    }
+}
