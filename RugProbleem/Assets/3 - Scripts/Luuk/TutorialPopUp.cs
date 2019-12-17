1147b1d5af24139383c3d3748184866ad9b06cbb
luuk boersma <49709526+Kluunkert@users.noreply.github.com>
2019-12-17 10:34:48 +0100

    Tutorial pop ups dunzo
    there dun

--- RugProbleem/Assets/3 - Scripts/Luuk/TutorialPopUp.cs
+++ RugProbleem/Assets/3 - Scripts/Luuk/TutorialPopUp.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class TutorialPopUp : MonoBehaviour
 {
     public List<GameObject> pep;
-    public List<bool> high; 
+    public List<bool> high;
+
+    public float timer = 90f;
+    public bool startTimer;
 
     public GameObject kluis;
 
@@ -15,8 +18,27 @@ public class TutorialPopUp : MonoBehaviour
         kluis = gameObject;
     }
 
+    public void Update()
+    {
+        if (startTimer == true && timer >= 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                Destroy(transform.GetChild(0).gameObject);
+                startTimer = false;
+            }
+        }
+    }
+
     public void Snuiven(int gram)
     {
-        Instantiate(pep[gram], transform.position, Quaternion.identity, kluis.transform);
+        if (high[gram] == false && timer >= 90f)
+        {
+            timer = 90f;
+            Instantiate(pep[gram], transform.position, Quaternion.identity, kluis.transform);
+            high[gram] = true;
+            startTimer = true;
+        }
     }
 }
